eceb90d84e1e33e5b35a674bc7fee421783bad36
Vijendar Komalla <vijendar.komalla@RACKSPACE.COM>
2013-06-13 14:33:41 -0500

    remove clouddbinstance from resource folder

--- heat/engine/resources/rackspace/rackspace_resource.py
+++ heat/engine/resources/rackspace/rackspace_resource.py
@@ -1,14 +1,31 @@
-from heat.engine import resource
-from heat.common import exception
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-from heat.openstack.common import log as logging
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 import pyrax
+from heat.engine import resource
+from heat.openstack.common import log as logging
 
 logger = logging.getLogger(__name__)
 
 
 class RackspaceResource(resource.Resource):
-        
+    '''
+    Common base class for Rackspace Resource Providers
+    '''
+    properties_schema = {}
+    
     def __init__(self, name, json_snippet, stack):
         super(RackspaceResource, self).__init__(name, json_snippet, stack)
         self.pyrax = pyrax
@@ -17,60 +34,67 @@ class RackspaceResource(resource.Resource):
         self._cloud_lb = None
         self._cloud_server = None
         self._cloud_nw = None
+        self._cloud_blockstore = None
 
-    @property
     def cloud_db(self):
-        if not self._cloud_db:
+        '''Rackspace cloud database client.'''      
+        if self._cloud_db:
             return self._cloud_db
         
-        self.authenticate()
+        self.__authenticate()
         self._cloud_db = self.pyrax.cloud_databases
         return self._cloud_db
     
-    @property
     def cloud_lb(self):
-        if not self._cloud_lb:
+        '''Rackspace cloud loadbalancer client.'''        
+        if self._cloud_lb:
             return self._cloud_lb
         
-        self.authnticate()
+        self.__authenticate()
         self._cloud_lb = self.pyrax.cloud_loadbalancers
         return self._cloud_lb
-    
-    @property
-    def cloud_server(self):
-        if not self._cloud_server:
+
+    def cloud_dns(self):
+        '''Rackspace cloud dns client'''        
+        if self._cloud_dns:
+            return self._cloud_dns
+         
+        self._cloud_dns = self.pyrax.cloud_dns
+        return self._cloud_dns
+   
+    def nova(self):
+        '''Rackspace nova client.'''
+        if self._cloud_server:
             return self._cloud_server
          
-        self.authenticate()
+        self.__authenticate()
         self._cloud_server = self.pyrax.cloudservers
         return self._cloud_server
 
-    @property
-    def cloud_dns(self):
-        if not self._cloud_dns:
-            return self._cloud_dns
-         
-        self.authenticate()
-        self._cloud_dns = self.pyrax.cloud_dns
-        return self._cloud_dns
-    
-    @property
-    def cloud_nw(self):
-        if not self._cloud_nw:
+    def cinder(self):
+        '''Rackspace cinder client.'''
+        if self._cloud_blockstore:
+            return self._cloud_blockstore
+        
+        self.__authenticate()
+        self._cloud_blockstore = self.pyrax.cloud_blockstorage
+        return self._cloud_blockstore
+
+    def quantum(self):
+        '''Rackspace quantum client.'''
+        if self._cloud_nw:
             return self._cloud_nw
         
-        self.authenticate()
+        self.__authenticate()
         self._cloud_nw = self.pyrax.cloud_networks
         return self._cloud_nw
-    
-    def authenticate(self):
-        print "/////////////////// Going to authenticate \\\\\\\\\\\\\\\\\\\\\\"
-        #self.rsusername = self.properties['RackspaceUserName']
-        #self.rsapikey =  self.properties['RackspaceApiKey']
+
+    def __authenticate(self):
         cls = self.pyrax.utils.import_class('pyrax.identity.rax_identity.RaxIdentity')
         self.pyrax.identity = cls()
-        self.pyrax.set_credentials(self.context.user, password=self.context.passowrd)
-        print "//////////////Authentication completed successfully\\\\\\\\\\\\\\\\"
+        logger.info("Authenticating with username:%s" % self.context.username)
+        self.pyrax.set_credentials(self.context.username, 
+                                   password=self.context.password)
+        logger.info("User %s authenticated successfully."
+                    % self.context.username)
         
-    def handle_update(self, json_snipped=None):
-        return self.UPDATE_REPLACE
