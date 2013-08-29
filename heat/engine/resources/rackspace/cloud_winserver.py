149ea89277147b3bf6c234e0633fefb31de9d53e
Vijendar Komalla <vijendar.komalla@RACKSPACE.COM>
2013-08-29 16:15:50 -0500

    fixing windows server resource
    Change-Id: I6c4bbc4fdeb0368f8a7677fa54b483d58eb8d37b

--- heat/engine/resources/rackspace/cloud_winserver.py
+++ heat/engine/resources/rackspace/cloud_winserver.py
@@ -1,5 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
@@ -12,184 +13,289 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import signal
+import subprocess
 import os
-import yaml
+import shlex
+import socket
+import time
+import tempfile
 
-from heat.common import urlfetch
+from heat.common import exception
+from heat.engine.resources.rackspace import rackspace_resource
+from heat.openstack.common import log as logging
 
-from heat.engine import environment
-from heat.engine import parser
-from heat.engine import resource
-from heat.engine.resources import template_resource
+logger = logging.getLogger(__name__)
 
-from heat.openstack.common import uuidutils
-
-from heat.tests import generic_resource as generic_rsrc
-from heat.tests.common import HeatTestCase
-from heat.tests import utils
-
-
-class MyCloudResource(generic_rsrc.GenericResource):
-    pass
+def alarm_handler(signum, frame):
+    raise Alarm
 
 
-class ProviderTemplateTest(HeatTestCase):
-    def setUp(self):
-        super(ProviderTemplateTest, self).setUp()
-        utils.setup_dummy_db()
-        resource._register_class('OS::ResourceType',
-                                 generic_rsrc.GenericResource)
-        resource._register_class('myCloud::ResourceType',
-                                 MyCloudResource)
+def run_command(cmd, lines=None, timeout=None):
+    p = subprocess.Popen(shlex.split(cmd),
+                         close_fds=True,
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
 
-    def test_get_os_empty_registry(self):
-        # assertion: with an empty environment we get the correct
-        # default class.
-        env_str = {'resource_registry': {}}
-        env = environment.Environment(env_str)
-        cls = env.get_class('OS::ResourceType', 'fred')
-        self.assertEqual(generic_rsrc.GenericResource, cls)
+    signal.signal(signal.SIGALRM, alarm_handler)
+    if timeout:
+        signal.alarm(timeout)
 
-    def test_get_mine_global_map(self):
-        # assertion: with a global rule we get the "mycloud" class.
-        env_str = {'resource_registry': {"OS::*": "myCloud::*"}}
-        env = environment.Environment(env_str)
-        cls = env.get_class('OS::ResourceType', 'fred')
-        self.assertEqual(MyCloudResource, cls)
+    try:
+        if lines:
+            (stdout, stderr) = p.communicate(input=lines)
 
-    def test_get_mine_type_map(self):
-        # assertion: with a global rule we get the "mycloud" class.
-        env_str = {'resource_registry': {
-            "OS::ResourceType": "myCloud::ResourceType"}}
-        env = environment.Environment(env_str)
-        cls = env.get_class('OS::ResourceType', 'fred')
-        self.assertEqual(MyCloudResource, cls)
+        status = p.wait()
+        signal.alarm(0)
+    except Alarm:
+        logger.warning("Timeout running post-build process")
+        status = 1
+        stdout = ''
+        p.kill()
 
-    def test_get_mine_resource_map(self):
-        # assertion: with a global rule we get the "mycloud" class.
-        env_str = {'resource_registry': {'resources': {'fred': {
-            "OS::ResourceType": "myCloud::ResourceType"}}}}
-        env = environment.Environment(env_str)
-        cls = env.get_class('OS::ResourceType', 'fred')
-        self.assertEqual(MyCloudResource, cls)
+    if lines:
+        output = stdout
 
-    def test_get_os_no_match(self):
-        # assertion: make sure 'fred' doesn't match 'jerry'.
-        env_str = {'resource_registry': {'resources': {'jerry': {
-            "OS::ResourceType": "myCloud::ResourceType"}}}}
-        env = environment.Environment(env_str)
-        cls = env.get_class('OS::ResourceType', 'fred')
-        self.assertEqual(generic_rsrc.GenericResource, cls)
+        # Remove this cruft from Windows build output
+        output = output.replace('\x08', '')
+        output = output.replace('\r', '')
 
-    def test_to_parameters(self):
-        """Tests property conversion to parameter values."""
-        utils.setup_dummy_db()
+    else:
+        output = p.stdout.read().strip()
 
-        class DummyResource(object):
-            attributes_schema = {"Foo": "A test attribute"}
-            properties_schema = {
-                "Foo": {"Type": "String"},
-                "AList": {"Type": "List"},
-                "ANum": {"Type": "Number"},
-                "AMap": {"Type": "Map"}
+    return (status, output)
+
+def get_wrapper_batch_file(command):
+    batch_file_command = "powershell.exe -executionpolicy unrestricted " \
+        "-command .\%s" % command
+    batch_file = tempfile.NamedTemporaryFile(suffix=".bat", delete=False)
+    batch_file.write(batch_file_command)
+    batch_file.close()
+    return batch_file.name
+    
+    
+def psexec_run_script(username, password, address, filename,
+                      command, path="C:\\Windows"):
+    psexec = "%s/psexec.py" % os.path.dirname(__file__)
+    psscript = "%s/download_wpi.ps1" % os.path.dirname(__file__)
+    cmd_string = "nice python %s -path '%s' '%s':'%s'@'%s' " \
+             "'c:\\windows\\sysnative\\cmd'"
+    cmd = cmd_string % (psexec, path, username, password, address)
+
+    # create a batch file that launches given powershell script
+    wrapper_batch_file = get_wrapper_batch_file(command)
+    lines = "put %s\nput %s\n%s\nexit\n" % (
+        filename, wrapper_batch_file, os.path.basename(wrapper_batch_file))
+
+    return run_command(cmd, lines=lines, timeout=1800)
+
+
+class CloudWinServer(rackspace_resource.RackspaceResource):
+    '''
+    Rackspace cloud Windows server resource.
+    '''
+    properties_schema = {
+        'name': {
+            'Type': 'String',
+            'Default': 'MyWindowsServer'
+            },
+
+        'flavor': {
+            'Type': 'String',
+            'Required': True            
+            },
+        
+        'image': {
+            'Type': 'String',
+            'Default': 'Windows Server 2012 (with updates)'
+            },
+        
+        'user_data': {
+            'Type': 'String',
+            'Default': 'None'
             }
+    }
 
-        env = environment.Environment()
-        resource._register_class('DummyResource', DummyResource)
-        env.load({'resource_registry':
-                  {'DummyResource': 'test_resource.template'}})
-        stack = parser.Stack(utils.dummy_context(), 'test_stack',
-                             parser.Template({}), env=env,
-                             stack_id=uuidutils.generate_uuid())
+    attributes_schema = {'PrivateDnsName': ('Private DNS name of the specified'
+                                            ' instance.'),
+                         'PublicDnsName': ('Public DNS name of the specified '
+                                           'instance.'),
+                         'PrivateIp': ('Private IP address of the specified '
+                                       'instance.'),
+                         'PublicIp': ('Public IP address of the specified '
+                                      'instance.')}
 
-        map_prop_val = {
-            "key1": "val1",
-            "key2": ["lval1", "lval2", "lval3"],
-            "key3": {
-                "key4": 4,
-                "key5": False
-            }
+    def __init__(self, name, json_snippet, stack):
+        super(CloudWinServer, self).__init__(name, json_snippet, stack)
+        self._private_ip = None
+        self._public_ip = None
+        self._server = None
+
+    @property
+    def server(self):
+        if not self._server:
+            logger.debug("Calling nova().servers.get()")
+            self._server = self.nova().servers.get(self.resource_id)
+        return self._server
+
+    def _get_ip(self, ip_type):
+        if ip_type in self.server.addresses:
+            for ip in self.server.addresses[ip_type]:
+                if ip['version'] == 4:
+                    return ip['addr']
+
+        raise exception.Error("Could not determine the %s IP of %s." %
+                              (ip_type, self.properties['image']))
+
+    @property
+    def public_ip(self):
+        """Return the public IP of the Cloud Server."""
+        if not self._public_ip:
+            self._public_ip = self._get_ip('public')
+
+        return self._public_ip
+
+    @property
+    def private_ip(self):
+        """Return the private IP of the Cloud Server."""
+        if not self._private_ip:
+            self._private_ip = self._get_ip('private')
+    
+        return self._private_ip
+
+    @property
+    def images(self):
+        """Get the images from the API."""
+        logger.debug("Calling nova().images.list()")
+        return [im.name for im in self.nova().images.list()]
+    
+    @property
+    def flavors(self):
+        """Get the flavors from the API."""
+        logger.debug("Calling nova().flavors.list()")
+        return [flavor.id for flavor in self.nova().flavors.list()]
+
+    def handle_create(self):
+        '''
+        Create Rackspace Cloud Windows Server Instance.
+        '''
+        logger.debug("CloudWinServer instance handle_create called")
+        serverinstancename = self.properties['name']
+        flavor = self.properties['flavor']
+        image = self.properties['image']
+
+        # create Windows server instance
+        logger.info("Creating Windows cloud server")
+        data = 'netsh advfirewall firewall add rule name="Port 445"' \
+            ' dir=in action=allow protocol=TCP localport=445'
+
+        files = {"C:\\cloud-automation\\bootstrap.bat": data,
+                 "C:\\cloud-automation\\bootstrap.cmd": data,
+                 "C:\\rs-automation\\bootstrap.bat": data,
+                 "C:\\rs-automation\\bootstrap.cmd": data}
+        imageRef = [im for im in self.nova().images.list()                    
+                    if im.name == image][0]
+        instance = self.nova().servers.create(serverinstancename,
+                                          imageRef,
+                                          flavor,
+                                          files = files)
+        if instance is not None:
+            self.resource_id_set(instance.id)
+
+        return instance
+
+    def check_create_complete(self, instance):
+        '''
+        Check if cloud Windows server instance creation is complete.
+        '''
+        instance.get()  # get updated attributes
+        if instance.status == 'ERROR':
+            instance.delete()
+            raise exception.Error("CloudWinServer instance creation failed.")
+
+        if instance.status != 'ACTIVE':
+            return False
+
+        logger.info("Cloud Windows server %s created (flavor:%s, image:%s)" %
+                    (instance.name,
+                     instance.flavor['id'],
+                     instance.image['id']))
+        
+        adminPass = instance.adminPass
+        user_data = self.properties['user_data']
+        # create powershell script with user_data
+        powershell_script = tempfile.NamedTemporaryFile(suffix=".ps1",
+                                                        delete=False)
+        
+        powershell_script.write(user_data)
+        ps_script_full_path = powershell_script.name
+        powershell_script.close()
+
+        # TODO: fix this sleep issue
+        time.sleep(8*60)
+        # Now connect to server using impacket and do the following
+        # 1. copy powershell script to remote server
+        # 2. execute the script
+        # 3. close the connection (exit)
+        (status, output) = psexec_run_script(
+            'Administrator',
+            adminPass,
+            self.public_ip,
+            ps_script_full_path,
+            os.path.basename(ps_script_full_path))
+        
+        # remove the temp powershell script
+        try:
+            os.remove(ps_script_full_path)
+        except:
+            pass
+        
+        return True
+
+    def handle_delete(self):
+        '''
+        Delete a Rackspace Cloud Windows Server Instance.
+        '''
+        logger.debug("CloudWinServer handle_delete called.")
+        if self.resource_id is None:
+            return
+
+        instance = self.nova().servers.get(self.resource_id)
+        instance.delete()
+        self.resource_id = None
+
+    def validate(self):
+        '''
+        Validate any of the provided params
+        '''
+        res = super(CloudWinServer, self).validate()
+        if res:
+            return res
+
+        # check validity of given image
+        if self.properties['image'] not in self.images:
+            return {'Error': 'Image not found.'}
+        
+        # check validity of gvien flavor
+        if self.properties['flavor'] not in self.flavors:
+            return {'Error': "flavor not found."}
+
+    def _resolve_attribute(self, name):
+        if name == 'PrivateIp':
+            return self.private_ip
+        elif name == 'PublicIp':
+            return self.public_ip
+        else:
+            return None
+
+
+# pyrax module is required to work with Rackspace cloud database provider.
+# If it is not installed, don't register clouddatabase provider
+def resource_mapping():
+    if rackspace_resource.PYRAX_INSTALLED:
+        return {
+            'Rackspace::Cloud::CloudWinServer': CloudWinServer,
         }
-        json_snippet = {
-            "Type": "DummyResource",
-            "Properties": {
-                "Foo": "Bar",
-                "AList": ["one", "two", "three"],
-                "ANum": 5,
-                "AMap": map_prop_val
-            }
-        }
-        self.m.ReplayAll()
-        temp_res = template_resource.TemplateResource('test_t_res',
-                                                      json_snippet, stack)
-        self.m.VerifyAll()
-        converted_params = temp_res._to_parameters()
-        self.assertTrue(converted_params)
-        for key in DummyResource.properties_schema:
-            self.assertIn(key, converted_params)
-        # verify String conversion
-        self.assertEqual("Bar", converted_params.get("Foo"))
-        # verify List conversion
-        self.assertEqual(",".join(json_snippet.get("Properties",
-                                                   {}).get("AList",
-                                                           [])),
-                         converted_params.get("AList"))
-        # verify Number conversion
-        self.assertEqual(5, converted_params.get("ANum"))
-        # verify Map conversion
-        self.assertEqual(map_prop_val, converted_params.get("AMap"))
-
-    def test_get_template_resource(self):
-        # assertion: if the name matches {.yaml|.template} we get the
-        # TemplateResource class.
-        env_str = {'resource_registry': {'resources': {'fred': {
-            "OS::ResourceType": "some_magic.yaml"}}}}
-        env = environment.Environment(env_str)
-        cls = env.get_class('OS::ResourceType', 'fred')
-        self.assertEqual(cls, template_resource.TemplateResource)
-
-    def test_template_as_resource(self):
-        """
-        Test that the resulting resource has the right prop and attrib schema.
-
-        Note that this test requires the Wordpress_Single_Instance.yaml
-        template in the templates directory since we want to test using a
-        non-trivial template.
-        """
-        test_templ_name = "WordPress_Single_Instance.yaml"
-        path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                            'templates', test_templ_name)
-        # check if its in the directory list vs. exists to work around
-        # case-insensitive file systems
-        self.assertIn(test_templ_name, os.listdir(os.path.dirname(path)))
-        with open(path) as test_templ_file:
-            test_templ = test_templ_file.read()
-        self.assertTrue(test_templ, "Empty test template")
-        self.m.StubOutWithMock(urlfetch, "get")
-        urlfetch.get(test_templ_name).AndReturn(test_templ)
-        parsed_test_templ = yaml.safe_load(test_templ)
-        self.m.ReplayAll()
-        json_snippet = {
-            "Type": test_templ_name,
-            "Properties": {
-                "KeyName": "mykeyname",
-                "DBName": "wordpress1",
-                "DBUsername": "wpdbuser",
-                "DBPassword": "wpdbpass",
-                "DBRootPassword": "wpdbrootpass",
-                "LinuxDistribution": "U10"
-            }
-        }
-        stack = parser.Stack(None, 'test_stack', parser.Template({}),
-                             stack_id=uuidutils.generate_uuid())
-        templ_resource = resource.Resource("test_templ_resource", json_snippet,
-                                           stack)
-        self.m.VerifyAll()
-        self.assertIsInstance(templ_resource,
-                              template_resource.TemplateResource)
-        for prop in parsed_test_templ.get("Parameters", {}):
-            self.assertIn(prop, templ_resource.properties)
-        for attrib in parsed_test_templ.get("Outputs", {}):
-            self.assertIn(attrib, templ_resource.attributes)
-        for k, v in json_snippet.get("Properties").items():
-            self.assertEqual(v, templ_resource.properties[k])
+    else:
+        return {}
